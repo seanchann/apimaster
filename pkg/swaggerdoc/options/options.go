3101fb9edfd65a9e69b040b19f4be3ecd78a73dd
seanchann <seanchann.zhou@gmail.com>
2018-08-21 09:46:31 +0800

    rewrite generate swagger doc

---
+++ pkg/swaggerdoc/options/options.go
@@ -0,0 +1,34 @@
+/*
+
+Copyright 2018 This Project Authors.
+
+Author:  seanchann <seanchann@foxmail.com>
+
+See docs/ for more information about the  project.
+
+*/
+
+package options
+
+import "github.com/spf13/pflag"
+
+//SwaggerDocOptions swagger doc options
+type SwaggerDocOptions struct {
+	FunctionDest string
+	TypeSrc      string
+	Verify       bool
+	HeaderFile   string
+}
+
+//NewSwaggerDocOptions new swagger options
+func NewSwaggerDocOptions() *SwaggerDocOptions {
+	return &SwaggerDocOptions{}
+}
+
+//AddFlags add flags
+func (s *SwaggerDocOptions) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&s.FunctionDest, "func-dest", "-", "Output for swagger functions; '-' means stdout (default)")
+	fs.StringVar(&s.TypeSrc, "type-src", "", "From where we are going to read the types")
+	fs.BoolVar(&s.Verify, "verify", false, "Verifies if the given type-src file has documentation for every type")
+	fs.StringVar(&s.HeaderFile, "header-file", "", "append your header file")
+}
---
+++ pkg/swaggerdoc/typedoc.go
@@ -0,0 +1,59 @@
+/*
+
+Copyright 2018 This Project Authors.
+
+Author:  seanchann <seanchann@foxmail.com>
+
+See docs/ for more information about the  project.
+
+*/
+
+package swaggerdoc
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/golang/glog"
+	"github.com/seanchann/apimaster/pkg/swaggerdoc/options"
+)
+
+//GenerateDoc generate swagger doc by options
+// visit https://github.com/emicklei/go-restful-swagger12 for detail
+func GenerateDoc(o *options.SwaggerDocOptions) {
+	if o.TypeSrc == "" {
+		glog.Fatalf("Please define -s flag as it is the source file")
+	}
+
+	var funcOut io.Writer
+	if o.FunctionDest == "-" {
+		funcOut = os.Stdout
+	} else {
+		file, err := os.Create(o.FunctionDest)
+		if err != nil {
+			glog.Fatalf("Couldn't open %v: %v", o.FunctionDest, err)
+		}
+		defer file.Close()
+		funcOut = file
+	}
+
+	docsForTypes := runtime.ParseDocumentationFrom(o.TypeSrc)
+
+	if o.Verify == true {
+		rc, err := runtime.VerifySwaggerDocsExist(docsForTypes, funcOut)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in verification process: %s\n", err)
+		}
+		os.Exit(rc)
+	}
+
+	if docsForTypes != nil && len(docsForTypes) > 0 {
+		if err := runtime.WriteSwaggerDocFunc(docsForTypes, funcOut); err != nil {
+			fmt.Fprintf(os.Stderr, "Error when writing swagger documentation functions: %s\n", err)
+			os.Exit(-1)
+		}
+	}
+}
